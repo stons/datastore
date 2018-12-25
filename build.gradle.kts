1735959fc96011d5b46c05a06fba1763f00cc63e
wangzhengtao <wangzhengtao@ggxueche.com>
2018-12-25 11:32:07 +0800

    add publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,11 @@ plugins {
     id("org.springframework.boot") version springBootVersion apply false
     id ("org.jetbrains.kotlin.kapt") version "1.3.10"
     id ("org.jetbrains.kotlin.jvm") version "1.3.10"
+    `maven-publish`
 }
+
+
+
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
@@ -37,6 +41,7 @@ subprojects{
     apply(plugin ="base")
     apply(plugin = "io.spring.dependency-management")
     apply(plugin  = "org.springframework.boot")
+    apply(plugin  = "maven-publish")
     tasks{
         "jar"(Jar::class){
             enabled = true
@@ -45,6 +50,56 @@ subprojects{
             enabled = false
         }
     }
+
+
+    task<Jar>("sourcesJar") {
+        classifier = "sources"
+        from(sourceSets.main.get().allJava)
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("binary") {
+                from(components["java"])
+            }
+            create<MavenPublication>("binaryAndSources") {
+                from(components["java"])
+                artifact(tasks["sourcesJar"])
+            }
+        }
+        repositories {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            maven {
+                name = "hhkj"
+                url = uri("http://192.168.1.222:8092/repository/maven-snapshots/")
+                credentials{
+                    username = "admin"
+                    password = "am@hao1!2"
+                }
+            }
+//            maven {
+//                name = "self"
+//                url = uri("$buildDir/repos/internal")
+//            }
+        }
+    }
+
+    tasks.withType<PublishToMavenRepository> {
+        onlyIf {
+            (repository == publishing.repositories["hhkj"] &&
+                    publication == publishing.publications["binary"])
+//                    ||
+//                    (repository == publishing.repositories["internal"] &&
+//                            publication == publishing.publications["binaryAndSources"])
+        }
+    }
+
+    tasks.withType<PublishToMavenLocal>() {
+        onlyIf {
+            publication == publishing.publications["binaryAndSources"]
+        }
+    }
+
 }
 
 project(":data-jpa"){
